51b403d742e660ce2e0237f2c2162e69d62de7e4
ahao <52927295@qq.com>
2025-07-17 20:54:40 +0800

    精简前端

--- frontend/src/components/dashboard/recent-projects.tsx
+++ frontend/src/components/dashboard/recent-projects.tsx
@@ -1,5 +1,5 @@
 import { FolderOpen } from 'lucide-react';
-import { getProjectIcon } from '../package/package-utils';
+import { getProjectIcon } from '@/components/package';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Project } from '@/types/simplified';
