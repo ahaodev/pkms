b683fffbdf673be2b1fb9a88567fc747b7199180
ahao <52927295@qq.com>
2025-07-21 10:57:30 +0800

    删除冗余

--- frontend/src/components/package/index.tsx
+++ frontend/src/components/package/index.tsx
@@ -1,12 +1,4 @@
-export { PackageGridCard } from './PackageGridCard';
-export { PackageListItem } from './PackageListItem';
-export { PackageList } from './PackageList';
-export { PackageHeader } from './package-header';
 export { PackageEmptyView } from './package-empty-view';
-export { PackageLoadingView } from './package-loading-view';
-export { PackageFilterControls } from './package-filter-controls';
 export { PackageCreateDialog } from './package-create-dialog.tsx';
-export { PackageReleaseCard } from './release/package-release-card.tsx';
-export { PackageReleaseHistoryDialog } from './release/package-release-history-dialog.tsx';
 export { PackageReleaseDialog } from './release/package-release-dialog.tsx';
 export * from './package-utils';
